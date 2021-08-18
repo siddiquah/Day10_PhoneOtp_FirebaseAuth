9f3440b28a0724c1c237bab56ff7d657f65be3a0
siddiquah anjum <siddiquahanjum111@gmail.com>
2021-08-18 23:01:05 +0530

    initial commit

---
+++ app/src/main/java/tech/hamid/day10_phoneotp_firebaseauth/HomeActivity.java
@@ -0,0 +1,14 @@
+package tech.hamid.day10_phoneotp_firebaseauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
